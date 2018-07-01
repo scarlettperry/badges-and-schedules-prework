104f591cd2fc24d8225ccb441a78e1ef191695dd
Scarlett Perry <si.perry15@gmail.com>
2018-07-01 16:33:53 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,8 +23,8 @@ def assign_rooms (attendees)
 end
 
 def printer
-  badge = batch_badge_creator(attendees)
-  room = assign_rooms(attendees)
+  badge = batch_badge_creator()
+  room = assign_rooms()
   
   bage.each do |name|
     puts name 
@@ -33,5 +33,4 @@ def printer
   rooms.each do |assigned|
     puts assigned
   end 
-end 
-
+end
